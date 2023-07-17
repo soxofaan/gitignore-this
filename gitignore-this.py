e0c6846e98044650c109e0f13db5090e9c0e87dc
Stefaan Lippens <soxofaan@gmail.com>
2023-07-17 17:17:53 +0200

    Basic working version

--- gitignore-this.py
+++ gitignore-this.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 
-import subprocess
 import argparse
+from pathlib import Path
+
+# TODO: don't add files that are already ignored
+# TODO: option to select .gitignore file (e.g. toplevel one vs subdir)
+# TODO: option to use glob instead of exact file name
+
+
+def gitignore(path: Path):
+    gitignore_file = path.parent / ".gitignore"
+    with gitignore_file.open("a", encoding="utf-8") as f:
+        f.write(f"{path.name}\n")
 
 
 def main():
     cli = argparse.ArgumentParser()
     cli.add_argument("file", nargs="*")
     cli_args = cli.parse_args()
-    print(cli_args)
+
+    for path in [Path(p) for p in cli_args.file]:
+        gitignore(path)
 
 
 if __name__ == "__main__":
