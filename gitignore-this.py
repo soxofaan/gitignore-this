634d3369b305bc3f240c9086e20618246465d354
Stefaan Lippens <soxofaan@gmail.com>
2023-07-17 17:25:57 +0200

    add --glob-stem option

--- gitignore-this.py
+++ gitignore-this.py
@@ -5,22 +5,33 @@ from pathlib import Path
 
 # TODO: don't add files that are already ignored
 # TODO: option to select .gitignore file (e.g. toplevel one vs subdir)
-# TODO: option to use glob instead of exact file name
+# TODO: option to automatically add newly created .gitignore to git staging
 
 
-def gitignore(path: Path):
+def gitignore(path: Path, glob_stem: bool = False):
     gitignore_file = path.parent / ".gitignore"
     with gitignore_file.open("a", encoding="utf-8") as f:
-        f.write(f"{path.name}\n")
+        if glob_stem:
+            entry = f"*{path.suffix}"
+        else:
+            entry = path.name
+        print(f"adding `{entry}` to `{gitignore_file}")
+        f.write(f"{entry}\n")
 
 
 def main():
     cli = argparse.ArgumentParser()
     cli.add_argument("file", nargs="*")
+    cli.add_argument(
+        "-g",
+        "--glob-stem",
+        action="store_true",
+        help="Ignore by file extension (replace stem with glob `*`).",
+    )
     cli_args = cli.parse_args()
 
     for path in [Path(p) for p in cli_args.file]:
-        gitignore(path)
+        gitignore(path, glob_stem=cli_args.glob_stem)
 
 
 if __name__ == "__main__":
